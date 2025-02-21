dd82955741b884471dae15aa0b03c0323f25866a
xeloc <c.dustin216@gmail.com>
2025-02-21 10:34:09 -0600

    Initial Commit
    Initial Commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string> 
+
+int main() {
+    
+    int myInt = 10;
+    float myFloat = 3.14f;
+    double myDouble = 2.71828;
+    char myChar = 'C';
+    bool myBool = true;
+    std::string myString = "Hello, World!";
+
+    
+    std::cout << "Integer: " << myInt << std::endl;
+    std::cout << "Float: " << myFloat << std::endl;
+    std::cout << "Double: " << myDouble << std::endl;
+    std::cout << "Character: " << myChar << std::endl;
+    std::cout << "Boolean: " << myBool << std::endl;
+    std::cout << "String: " << myString << std::endl;
+
+    return 0;
+}
